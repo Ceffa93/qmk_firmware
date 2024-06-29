9179336b538132d7933a0da1d7321b7eda381e43
Ceffa93 <ceffa93@gmail.com>
2024-06-29 19:54:26 +0900

    Fixed issue with double tap

--- keyboards/yushakobo/ergo68/keymaps/filippo/keymap.c
+++ keyboards/yushakobo/ergo68/keymaps/filippo/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[3][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,      KC_8,        KC_9,       KC_0,       S(KC_CAPS),
         KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_MINS,    KC_EQL,     KC_J,       KC_L,      KC_U,        KC_Y,       KC_SCLN,    KC_NO,
         KC_NO,      MOD_LG,     MOD_LA,     MOD_LC,     MOD_LS,     KC_G,       KC_LBRC,    KC_RBRC,    KC_M,       MOD_RS,    MOD_RC,      MOD_RA,     MOD_RG,     KC_GRV,
-        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       KC_QUOT,    KC_BSLS,    KC_K,       KC_H,      KC_COMM,     KC_DOT,     KC_SLSH,    KC_NO,
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       KC_QUOT,    KC_BSLS,    KC_K,       KC_H,      KC_COMM,     KC_DOT,     KC_SLSH,    RGB_TOG,
         KC_LCTL,    KC_NO,      KC_NO,      KC_NO,      KC_DEL,     KC_BSPC,    MO(1),      KC_ENT,     KC_SPC,     MO(2),     KC_SPC,      KC_NO,      KC_NO,      QK_BOOT
     ),
     [1] = LAYOUT(
