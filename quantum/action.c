f0c3d05765e952653e62d658e908ada81abf739a
Ceffa93 <ceffa93@gmail.com>
2025-07-13 10:36:32 +0900

    reintegrated my retrotapping fix, and removed achordion

--- quantum/action.c
+++ quantum/action.c
@@ -531,7 +531,7 @@ void process_action(keyrecord_t *record, action_t action) {
 #    if defined(RETRO_TAPPING) && defined(DUMMY_MOD_NEUTRALIZER_KEYCODE)
                             // Send a dummy keycode to neutralize flashing modifiers
                             // if the key was held and then released with no interruptions.
-                            if (retro_tapping_counter == 2) {
+                            if (retro_tapping_counter >= 2) { // FILIPPO CHANGE
                                 neutralize_flashing_modifiers(get_mods());
                             }
 #    endif
